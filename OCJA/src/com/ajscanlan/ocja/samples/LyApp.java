c756ade5a549028dc1346847213135ce5deb48c4
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-13 15:43:14 +0100

    Messing with strings

---
+++ OCJA/src/com/ajscanlan/ocja/samples/LyApp.java
@@ -0,0 +1,14 @@
+package com.ajscanlan.ocja.samples;
+
+public class LyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "oddly";
+		
+		if(str.substring(str.length() - 2, str.length()).equals("ly")) System.out.println("true");
+		
+	//	System.out.println(str.substring(str.length() - 2, str.length()));
+	}
+
+}
