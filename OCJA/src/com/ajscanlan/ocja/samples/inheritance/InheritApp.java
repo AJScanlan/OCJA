43a6979884cfc2d06cf2bbc3aee671b3e3b1ec9c
Alex Scanlan <ajscanlan27@gmail.com>
2015-08-04 11:53:48 +0100

    Added comments

--- OCJA/src/com/ajscanlan/ocja/samples/inheritance/InheritApp.java
+++ OCJA/src/com/ajscanlan/ocja/samples/inheritance/InheritApp.java
@@ -8,24 +8,24 @@ public class InheritApp {
 
 	private void runProgram() {
 		
+		//testing person
 		Person alex = new Person("Alex");
-		
 		this.makeSomeNoise(alex);
 		
+		//testing cat
 		Cat meowth = new Cat("Meowth");
-		
 		this.makeSomeNoise(meowth);
 		
+		//testing dog
 		Dog dog = new Dog("Taz");
-		
 		this.makeSomeNoise(dog);
-		
+
+		//testing no args
 		Person def = new Person();
-		
 		this.makeSomeNoise(def);
 		
+		//testing dog
 		Duck duck = new Duck("Don");
-		
 		this.makeSomeNoise(duck);
 	}
 
@@ -33,6 +33,8 @@ public class InheritApp {
 		a.makeNoise();
 	}
 
+	
+	//deprecated methods
 //	private void makeSomeNoiseCats(Cat c){
 //		c.makeNoise();
 //	}
